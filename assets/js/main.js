bcaa93a29dd1be1998d63039a06392a2602ee8e9
shahryar-r <shahryarrashidi@yahoo.co.uk>
2026-06-17 01:11:11 +0100

    Add files via upload

--- assets/js/main.js
+++ assets/js/main.js
@@ -226,25 +226,6 @@
 
 			});
 
-/*
-
-	// Menu.
-		$('#menu')
-			.append('<a href="#menu" class="close"></a>')
-			.appendTo($body)
-			.panel({
-				delay: 500,
-				hideOnClick: true,
-				hideOnSwipe: true,
-				resetScroll: true,
-				resetForms: true,
-				side: 'right',
-				target: $body,
-				visibleClass: 'is-menu-visible'
-			});
-
-*/
-
 		}
 
 })(jQuery);
